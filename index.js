f3054b1419fc680316a9727534ba9e22cff04bf5
ChickenChamp71 <chickensandsharks@gmail.com>
2023-05-21 23:52:30 -0700

    completed markdown generation, now creates an output file with info

--- index.js
+++ index.js
@@ -1,25 +1,81 @@
 // TODO: Include packages needed for this application
 const generateMarkdown = require('./utils/generateMarkdown');
 const inquirer = require('inquirer');
-const {writeFile} = require('fs').promises;
 const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = ['What is your project title?','What is your description?','What is in your table of contents?','How do you install your project?','What is your project used for?','Which of these licenses did you use?','Who contributed to this project?','What tests were preformed?'];
+const questions = ['What is your project title?','What is your description?','How do you install your project?','What is your project used for?','Which of these licenses did you use?','Who contributed to this project?','What tests were preformed?','What is your GitHub username?','What is your email?'];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    js.writeFile()
+function writeToFile(data) {
+
+    fs.writeFile("README-output.md", generateMarkdown(data), (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('README file successfully created!');
+        }
+    }
+    );
 }
 
 // TODO: Create a function to initialize app
 function init() {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'title',
-            message:
-        }
-    ])
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'title',
+                message: questions[0],
+            },
+            {
+                type: 'input',
+                name: 'description',
+                message: questions[1],
+            },
+            {
+                type: 'input',
+                name: 'install',
+                message: questions[2],
+            },
+            {
+                type: 'input',
+                name: 'use',
+                message: questions[3],
+            },
+            {
+                type: 'input',
+                name: 'contributing',
+                message: questions[5],
+            },
+            {
+                type: 'input',
+                name: 'tests',
+                message: questions[6],
+            }, 
+            {
+                type: 'list',
+                name: 'license',
+                message: questions[4],
+                choices: ['IBM','ISC','MIT','None'],
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: questions[7],
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: questions[8],
+            }
+        ])
+        .then((answers) => {
+
+            const data = answers;
+
+            writeToFile(data);
+
+        })
 }
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,78 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+var licenseBadge = '';
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function checkLicense(data) {
+  console.log(data);
+  if (data.license === 'IBM') {
+    licenseBadge = '[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)';
 
-// TODO: Create a function to generate markdown for README
+    console.log('IBM');
+
+  } else if (data.license === 'ISC') {
+    licenseBadge = '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)';
+
+    console.log('ISC');
+
+  } else if (data.license === 'MIT') {
+    licenseBadge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+
+    console.log('MIT');
+
+  } else {
+    licenseBadge = '';
+
+    console.log('None');
+  }
+}
+
 function generateMarkdown(data) {
-  return `# ${data.title}
 
-`;
+  checkLicense(data);
+
+  return `# ${data.title}
+  ${licenseBadge}
+
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Installation
+
+  ${data.install}
+
+  ## Usage
+
+  ${data.use}
+
+  ## License
+
+  ${data.license}
+
+  ## Contributing
+
+  ${data.contributing}
+
+  ## Tests
+
+  ${data.tests}
+
+  ## Questions
+
+  ${data.github}
+
+  https://github.com/${data.github}
+
+  Email: ${data.email}`;
+
 }
 
 module.exports = generateMarkdown;
